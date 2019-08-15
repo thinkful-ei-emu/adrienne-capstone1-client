a2f52baac68ca6d1c7cd591f9a69e2643ce9572d
Adrienne Smith <agsmith046@gmail.com>
2019-08-15 17:13:52 -0500

    switched back to pointing to correct database

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // API_ENDPOINT: 'https://gentle-garden-12660.herokuapp.com/api',
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://gentle-garden-12660.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
   TOKEN_KEY: 'token',
 }
